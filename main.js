94152cc09b8d5c946de488875e6683f8ecdc3eb0
sunnyiuww <126841766+sunnyiuww@users.noreply.github.com>
2024-03-24 23:38:01 +0800

    Update main.js

--- main.js
+++ main.js
@@ -37,3 +37,25 @@ var typingTextElement = document.getElementById("typing-text-content");
 typeEffect(typingTextElement, textToType, typingSpeed, deleteSpeed, typingInterval);
 
 typingTextElement.style.color = "#9400FF";
+
+
+// Inisialisasi ScrollReveal
+const sr = ScrollReveal({
+    distance: '80px',
+    duration: 2000,
+    delay: 200,
+});
+
+// Menerapkan efek reveal pada setiap elemen yang diinginkan
+sr.reveal('.blob1, .blob2', { origin: 'top' });
+sr.reveal('.container > div', { origin: 'bottom', interval: 200 });
+sr.reveal('.home-img, .educations-container, .certificates-box, .contact form', { origin: 'bottom', interval: 200 });
+sr.reveal('.home-contact h1, .about-img', { origin: 'left' });
+sr.reveal('.home-contact p, .about-content', { origin: 'right' });
+sr.reveal('.user-pic', { origin: 'bottom', interval: 200 });
+
+// Menambahkan efek reveal pada elemen teks <h4> di bagian "About"
+sr.reveal('.container > div > h4, .container > h4.title', { origin: 'bottom', interval: 200 });
+
+// Menambahkan efek reveal pada bagian "Contact"
+sr.reveal('#contact .container > div, #contact form', { origin: 'bottom', interval: 200 });
